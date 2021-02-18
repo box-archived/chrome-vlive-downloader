59823b3ba66812e057b742aabb026a6f16222806
box-archived <box.cassette@gmail.com>
2021-02-18 10:38:00 +0900

    Add copyright text to codes

--- core.js
+++ core.js
@@ -1,3 +1,18 @@
+/*
+ * VLIVE Downloader Chrome-extension by box_archived
+ * Copyright (c) 2020- box_archived
+ *
+ * VLIVE Downloader is free software under license GPL-3.0
+ * You can modify and redistribute the software by follow the license
+ * Check source code at github.com
+ * <https://github.com/box-archived/chrome-vlive-downloader>
+ *
+ * For only personal purpose, you can download video.
+ * However, if you share or redistribute downloaded video,
+ * it could be a copyright infringement on VLIVE.
+ * We do not guarantee any legal responsibility for the use.
+ */
+
 appId = "8c6cc7b45d2568fb668be6e05b6e5a3b";
 (function () {
     // Functions
--- streamDownload.js
+++ streamDownload.js
@@ -1,3 +1,18 @@
+/*
+ * VLIVE Downloader Chrome-extension by box_archived
+ * Copyright (c) 2020- box_archived
+ *
+ * VLIVE Downloader is free software under license GPL-3.0
+ * You can modify and redistribute the software by follow the license
+ * Check source code at github.com
+ * <https://github.com/box-archived/chrome-vlive-downloader>
+ *
+ * For only personal purpose, you can download video.
+ * However, if you share or redistribute downloaded video,
+ * it could be a copyright infringement on VLIVE.
+ * We do not guarantee any legal responsibility for the use.
+ */
+
 let blobObj;
 let blobUrl;
 let downloadedObj = {};
