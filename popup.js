3adb365b0fec0b7b62af39f4204998d34fa093f3
box-archived <box.cassette@gmail.com>
2021-02-18 16:11:31 +0900

    Add popup base

---
+++ popup.js
@@ -0,0 +1,81 @@
+/*
+ * VLIVE Downloader Chrome-extension by box_archived
+ * Copyright (c) 2020- box_archived
+ *
+ * VLIVE Downloader is free software under license GPL-3.0
+ * You can modify and redistribute the software by follow the license
+ * Check source code at github.com
+ * <https://github.com/box-archived/chrome-vlive-downloader>
+ *
+ * For only personal purpose, you can download video.
+ * However, if you share or redistribute downloaded video,
+ * it could be a copyright infringement on VLIVE.
+ * We do not guarantee any legal responsibility for the use.
+ */
+
+function i18nLoader() {
+    const elementList = document.querySelectorAll("[data-i18n]");
+    elementList.forEach(function (element) {
+        element.innerText = chrome.i18n.getMessage(element.dataset.i18n)
+    })
+}
+
+function hideSpinner() {
+    document.querySelector("#spinner").classList.add("disappear");
+}
+
+function sendAlert(i18n, type) {
+    if(!type) {
+        type = 'primary'
+    }
+    hideSpinner();
+    document.querySelector("#result").innerHTML = `<div class="alert alert-${type}" data-i18n="${i18n}">${i18n}</div>`;
+}
+
+function renderDOM(vdResult) {
+    let html = ``;
+    let postModel =
+        ``;
+
+    return html
+}
+
+async function main() {
+    function resultCheck() {
+        return new Promise(function (resolve) {
+            const checker = setInterval(function () {
+                chrome.tabs.executeScript(null, {code: 'window.__VD_RESULT__'}, function (vdResult) {
+                    if(vdResult[0].working === false) {
+                        console.log(vdResult[0]);
+                        clearInterval(checker);
+                        resolve(vdResult)
+                    }
+                })
+            }, 100)
+        });
+    }
+
+
+    await new Promise(function(resolve) {
+        chrome.tabs.executeScript(null, {file: 'core.js'}, function () {
+                resolve();
+        });
+    }).then(() => {});
+    const vdResult = await resultCheck();
+
+    document.querySelector('#result').innerHTML = renderDOM(vdResult);
+    hideSpinner()
+}
+
+window.onload = function () {
+    new Promise(function (resolve) {
+        chrome.tabs.getSelected(null, function(tab) {
+            if(tab.url.search(/(?<=chrom)[\-a-z]*:/g) !== -1) {
+                sendAlert("alert_unusable");
+                resolve()
+            } else {
+                main().then(() => {resolve()});
+            }
+        });
+    }).then(i18nLoader)
+};
