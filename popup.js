b1d51ea72774b999f5057ed5f91882d7e67d9bb9
box-archived <box.cassette@gmail.com>
2021-03-04 07:52:02 +0900

    Fix srt this property error

--- popup.js
+++ popup.js
@@ -27,7 +27,7 @@ function chromeDownload(obj) {
 function srtDownload(obj) {
     const vttLoad = new Promise(function (resolve, reject) {
         const xhr = new XMLHttpRequest();
-        xhr.open("GET", obj.target.dataset.url);
+        xhr.open("GET", obj.dataset.url);
 
         // Set result
         xhr.onload = function () {
@@ -46,7 +46,7 @@ function srtDownload(obj) {
         result = result.replaceAll(/(?<=\d{2}:\d{2}:\d{2}).(?=\d{3})/gm, ",");
         const blobObj = new Blob([result], {type: 'application/x-subrip'});
         const blobUrl = window.URL.createObjectURL(blobObj);
-        chrome.downloads.download({url: blobUrl, filename: obj.target.dataset.name}, () => {URL.revokeObjectURL(blobUrl)});
+        chrome.downloads.download({url: blobUrl, filename: obj.dataset.name}, () => {URL.revokeObjectURL(blobUrl)});
     });
 }
 
